958ec0054f603d5f86ae9022524ec686e21d6073
Christian Baumann <chriss713@gmail.com>
2021-04-04 11:08:58 +0200

    Added generated code from spring initializr

---
+++ src/main/java/de/chrissbaumann/hadata/HaDataApplication.java
@@ -0,0 +1,13 @@
+package de.chrissbaumann.hadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/chrissbaumann/hadata/HaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package de.chrissbaumann.hadata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
